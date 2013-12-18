b952516e40f46db78325d92089da07494e5d3e28
Soheil <ysoheil@gmail.com>
2013-12-18 15:48:46 -0800

    keep tar gz files longer by using sha of gemfile as part of the tar.gz filename

--- command_to_execute.sh
+++ command_to_execute.sh
@@ -1,8 +1,7 @@
 export RAILS_ENV=test
 export GEM_PATH=./vendor/bundle
-export GEMS_TAR_FILE=/tmp/JOB_NAME_gems.tar.gz
-export GEMFILE_SHA_FILE=/tmp/JOB_NAME_gemfile.sha
-export GEMS_SHA_FILE=/tmp/JOB_NAME_gems.sha
+export GEMFILE_SHA_CALCULATED="$(shasum Gemfile | cut -f1 -d' ')"
+export GEMS_TAR_FILE=/tmp/"$GEMFILE_SHA_CALCULATED"_gems.tar.gz
 
 git clone --depth 1 GIT_REMOTE_ORIGIN .
 git checkout GIT_BRANCH
@@ -15,14 +14,7 @@ mv /tmp/database.yml config/database.yml
 cat config/tire.yml | sed 's/test_/JOB_NAME_test_/g' > /tmp/tire.yml
 mv /tmp/tire.yml config/tire.yml
 
-export GEMS_SHA_CALCULATED="$(shasum $GEMS_TAR_FILE)"
-export GEMS_SHA="$(cat $GEMS_SHA_FILE)"
-export GEMFILE_SHA_CALCULATED="$(shasum Gemfile)"
-export GEMFILE_SHA="$(cat $GEMFILE_SHA_FILE)"
-export GEM_FILES_EXIST=$([ -f "$GEMS_TAR_FILE" ] && [ -f "$GEMS_SHA_FILE" ] && [ -f "$GEMFILE_SHA_FILE" ] && [ -f "/tmp/JOB_NAME_gems.tar.gz" ] && echo 1)
-export HAS_GEMS_TARBALL=$([ -n "$GEM_FILES_EXIST" ] && [ "$GEMS_SHA_CALCULATED" = "$GEMS_SHA" ] && [ "$GEMFILE_SHA_CALCULATED" = "$GEMFILE_SHA" ] && echo 1)
-
-if [ -n "$HAS_GEMS_TARBALL" ]
+if [ -f "$GEMS_TAR_FILE" ]
 then mkdir "$GEM_PATH"
   cd "$GEM_PATH"
   tar -zxvf $GEMS_TAR_FILE
@@ -34,9 +26,11 @@ else bundle --deployment
   cd "$GEM_PATH"
   tar -zcvf $GEMS_TAR_FILE .
   cd -
-  shasum $GEMS_TAR_FILE > $GEMS_SHA_FILE
-  shasum Gemfile > $GEMFILE_SHA_FILE
   echo 'gems updated'
+  cd /tmp
+  tarballCount=$(ls -lt | grep _gems.tar.gz | wc -l | sed 's/ //g')
+  [[ $tarballCount > 5 ]] && ls -t | grep _gems.tar.gz | tail -n$(($tarballCount - 5)) | xargs rm
+  cd -
 fi
 
 [ -d "coverage" ] && rm -rf coverage
